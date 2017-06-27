0d49660f2e97dc9f4661e02bca259ba65302f4dc
Michael Dinh <mkdinh94@gmail.com>
2017-06-26 22:57:49 -0400

    adding localStorage on tags

--- assets/javascript/giphyAPI.js
+++ assets/javascript/giphyAPI.js
@@ -1,20 +1,24 @@
 	$(document).ready(function(){
 
 
-var updatedTag = ["soccer","sock","watermelon","chocolate","italian food","spongebob"];
-var initialTag = true;
+var updatedTag;
 
-function updateTag(){
-	$("#tagPanel").empty();
-	for(i = 0; i < updatedTag.length; i++){
-		var btn = $("<button>");
-		btn.attr("tag",updatedTag[i]);
-		btn.addClass(" tag btn btn-primary");
-		btn.text(updatedTag[i]);
-		$("#tagPanel").append(btn)
-		initialTag = false;
-	}
+if(Array.isArray(JSON.parse(localStorage.getItem("tags")))){
+	console.log(updateTag)
+	console.log("from localStorage")
+	updatedTag = JSON.parse(localStorage.getItem("tags"))
+}else{
+	console.log("from HTML")
+	console.log(updatedTag)
+	updatedTag = ["soccer","sock","watermelon","chocolate","italian food","spongebob"];
+	localStorage.setItem("tags",JSON.stringify(updatedTag))
 }
+
+var initialTag = true;
+var topic;
+var limit;
+var rating;
+var response;
 
 if(initialTag == true){
 	 updateTag();
@@ -25,11 +29,11 @@ $("#submit").click(function(event){
     event.preventDefault();
 
 
-    var apiKey = "dc6zaTOxFJmzC";
-	var queryURL = "https://api.giphy.com/v1/gifs/search";
-	var topic = $("#searchTag").val().trim();
-	var limit = $("#limit option:selected").text();
-	var rating = $("#rating option:selected").text();
+    apiKey = "dc6zaTOxFJmzC";
+	queryURL = "https://api.giphy.com/v1/gifs/search";
+	topic = $("#searchTag").val().trim();
+	limit = $("#limit option:selected").text();
+	rating = $("#rating option:selected").text();
 
     if(topic === ""){return}
 
@@ -46,41 +50,20 @@ $("#submit").click(function(event){
 	}).done(function(response){
 		if(updatedTag.indexOf(topic) === -1){
 		updatedTag.push(topic);
+		localStorage.setItem("tags",JSON.stringify(updatedTag))
+		console.log(updatedTag)
 		updateTag();
 		}
-		console.log(response)
-		var panel = $("<div>");
-		panel.addClass("panel panel-default");
-		panel.attr("name-data",topic)
-
-		var panelHead = $("<div>");
-		panelHead.addClass("panel-heading")
-		panelHead.html("<b>Topic: </b>" + topic+ "&nbsp; &nbsp; &nbsp;" + "<b>Limit: </b>" + limit + "&nbsp; &nbsp; &nbsp;" + "<b>Rating: </b>" + rating);
-		panel.append(panelHead);
-
-		var panelBody = $("<div>");
-		panelBody.addClass("panel-body");
-		for(i = 0; i < response.data.length; i++){
-			var imgEl = $("<img>");
-			imgEl.addClass("gif img-thumbnail")
-			imgEl.attr("data-state","still")
-			imgEl.attr("data-still",response.data[i].images.fixed_width_still.url)
-			imgEl.attr("data-animate",response.data[i].images.fixed_width.url)
-			imgEl.attr("src",response.data[i].images.fixed_width_still.url)
-			panelBody.append(imgEl)
-		}
-
-		panel.append(panelBody);
-		panel.hide();
-		$("#results").prepend(panel)
-		panel.fadeIn(1000)
+		
+		updateGIF(response)
 	})
 })
 
 $("#clear").click(function(){
        //clear form and previous search
         $("#searchTag").empty();
-        //clear previous search
+        localStorage.clear();
+
         $("#results").empty();
         
         //empty search box
@@ -96,11 +79,15 @@ $("#clear").click(function(){
 
 	$('body').on('click', '.gif', function (){
 		if($(this).attr("data-state") === "still"){
-			console.log($(this).attr("data-state"))
+			var ratingState = $(this).attr("data-num")
+			console.log()
+			$("#"+ratingState).css('display',"none")
 			$(this).attr("src",$(this).attr("data-animate"))
 			$(this).attr("data-state",'animate')
 			console.log($(this).attr("data-animate"))	
 		}else{
+			var ratingState = $(this).attr("data-num")
+			$("#"+ratingState).css('display',"block")
 			$(this).attr("src",$(this).attr("data-still"))
 			$(this).attr("data-State","still"); 
 		}
@@ -110,53 +97,125 @@ $("#clear").click(function(){
 		event.preventDefault();
 
 
-    var apiKey = "dc6zaTOxFJmzC";
-	var queryURL = "https://api.giphy.com/v1/gifs/search";
-	var topic = $(this).attr("tag");
-	var limit = $("#limit option:selected").text()
-	var rating = $("#rating option:selected").text();
+    apiKey = "dc6zaTOxFJmzC";
+	queryURL = "https://api.giphy.com/v1/gifs/";
+	if ($(this).attr("tag") === "random"){
+		queryURL += "random"
+	}else if($(this).attr("tag") === "trending"){
+		queryURL += "trending"
+	}else{
+		queryURL += "search"
+	}
 
-	console.log(topic)
-	queryURL += "?" + $.param({
+	topic = $(this).attr("tag");
+	limit = $("#limit option:selected").text()
+	rating = $("#rating option:selected").text();
+
+	if($(this).attr("tag") === "trending"){
+		queryURL += "?" + $.param({
 		    "api_key": apiKey,
-	        "q": topic,
 	        'limit': limit,
 	        'rating': rating,
-	})
-
+		})
+	}else if ($(this).attr("tag") === "random"){
+		queryURL += "?" + $.param({
+		    "api_key": apiKey,
+	        'rating': rating,
+		})
+	}else{
+		queryURL += "?" + $.param({
+	    "api_key": apiKey,
+        "q": topic,
+        'limit': limit,
+        'rating': rating,
+		})
+	}
+	console.log(queryURL)
     $.ajax({
     url: queryURL,
     method: "GET"
 	}).done(function(response){
-		console.log(this)
-		var panel = $("<div>");
-		panel.addClass("panel panel-default");
-		panel.attr("name-data",topic)
 
-		var panelHead = $("<div>");
-		panelHead.addClass("panel-heading")
-		panelHead.html("<b>Tag: </b>" + topic+ "&nbsp; &nbsp; &nbsp;" + "<b>Limit: </b>" + limit + "&nbsp; &nbsp; &nbsp;" + "<b>Rating: </b>" + rating);
-		panel.append(panelHead);
+		updateGIF(response)
 
-		var panelBody = $("<div>");
-		panelBody.addClass("panel-body");
+		})
+	})
+
+function updateTag(){
+	$("#tagPanel").empty();
+	updatedTag = JSON.parse(localStorage.getItem("tags"));
+	for(i = 0; i < updatedTag.length; i++){
+		var btn = $("<button>");
+		btn.attr("tag",updatedTag[i]);
+		btn.addClass(" tag btn btn-primary");
+		btn.text(updatedTag[i]);
+		$("#tagPanel").append(btn)
+		initialTag = false;
+
+	}
+}
+
+function updateGIF(response){
+	var panel = $("<div>");
+	panel.addClass("panel panel-default");
+	panel.attr("name-data",topic)
+
+	var panelHead = $("<div>");
+	panelHead.addClass("panel-heading")
+	panel.append(panelHead);
+	console.log(response)
+	var panelBody = $("<div>");
+	panelBody.addClass("panel-body");
+	if (topic === "random"){
+		panelHead.html("<b>Tag: </b>" + topic)
+		var imgContainer = $("<div>");
+		imgContainer.attr("data-name",topic);
+		imgContainer.addClass("imgContainer");
+		imgContainer.css("width","100%");
+		// var ratingText = $("<div>");
+		// ratingText.addClass("ratingText");
+		// ratingText.attr("id","randomGIF")
+		// ratingText.text(response.data.rating.toUpperCase())
+		var imgEl = $("<img>");
+		imgEl.attr("data-num","randomGIF")
+		imgEl.addClass("gif img-thumbnail")
+		imgEl.attr("data-state","still")
+		imgEl.attr("data-still",response.data.fixed_height_small_still_url)
+		imgEl.attr("data-animate",response.data.fixed_height_small_url)
+		imgEl.attr("src",response.data.fixed_width_downsampled_url)
+		imgEl.addClass("randomGIF")
+		console.log(response.data.fixed_height_small_still_url)
+		imgContainer.append(imgEl)
+		// imgContainer.append(ratingText)
+		panelBody.append(imgContainer)
+	}else{
 		for(i = 0; i < response.data.length; i++){
+		panelHead.html("<b>Tag: </b>" + topic+ "&nbsp; &nbsp; &nbsp;" + "<b>Limit: </b>" + limit + "&nbsp; &nbsp; &nbsp;" + "<b>Rating: </b>" + rating);
+			var imgContainer = $("<div>");
+			imgContainer.attr("data-name",topic);
+			imgContainer.addClass("imgContainer");
+			var ratingText = $("<div>");
+			ratingText.addClass("ratingText");
+			ratingText.attr("id","item-"+i)
+			ratingText.text(response.data[i].rating.toUpperCase())
 			var imgEl = $("<img>");
+			imgEl.attr("data-num","item-"+i)
 			imgEl.addClass("gif img-thumbnail")
 			imgEl.attr("data-state","still")
 			imgEl.attr("data-still",response.data[i].images.fixed_width_still.url)
 			imgEl.attr("data-animate",response.data[i].images.fixed_width.url)
 			imgEl.attr("src",response.data[i].images.fixed_width_still.url)
-			panelBody.append(imgEl)
+			imgContainer.append(imgEl)
+			imgContainer.append(ratingText)
+			panelBody.append(imgContainer)
 		}
+	}
+	panel.append(panelBody);
+	panel.hide();
+	$("#results").prepend(panel);
+	$("#results").prepend("<hr>");
+	panel.fadeIn(1000)
 
-		panel.append(panelBody);
-		panel.hide();
-		$("#results").prepend(panel)
-		panel.fadeIn(1000)
+}
 
-		})
-	})
-
-})
-
+});
